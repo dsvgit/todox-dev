226478ae47ce44806f9667c3786c6723a04fcc6d
Elvira Salakhova <elvirkin1392@gmail.com>
2024-11-19 14:13:16 +0500

    make items editable

--- src/components/todo-item.js
+++ src/components/todo-item.js
@@ -6,9 +6,12 @@ export const TodoItem = ({ $todo, onRemove }) => {
     e.preventDefault();
     $todo.value = { ...$todo.value, checked: e.target.checked };
   };
-
+  const handleEdit = (e) => {
+    e.preventDefault();
+    $todo.value = { ...$todo.value, text: e.target.innerText };
+  };
   return render`
-    <li class="list-group-item list-group-item-action hstack gap-3 pe-1 align-items-start">
+    <li class="list-group-item todox-list-group-item-action hstack gap-3 pe-1 align-items-start">
       ${render`
         <input id="${id}" type="checkbox" class="form-check-input" />
         ${(element) => {
@@ -22,11 +25,23 @@ export const TodoItem = ({ $todo, onRemove }) => {
       `}
       
       ${render`
-        <label for="${id}" class="d-inline form-check-label flex-fill" />
+        <div class="d-inline flex-fill todox-item-editable" contenteditable="true"/>
         ${(element) => {
+          let isFocused = false;
+          element.addEventListener("input", handleEdit);
+          element.addEventListener("focus", () => {
+            isFocused = true;
+          });
+          element.addEventListener("blur", () => {
+            isFocused = false;
+          });
+
           effect(() => {
             const { text, checked } = $todo.value;
-            element.textContent = text;
+            if (!isFocused) {
+              element.innerText = text;
+            }
+
             element.style.opacity = checked ? 0.5 : 1;
           });
         }}
